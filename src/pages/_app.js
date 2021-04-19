b5d3f473787a81553f9d4b7c78257a13ab1662f3
Keriz <guillaume.thivolet@free.fr>
2021-04-19 14:52:20 +0200

    game

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,6 +9,7 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta name="msapplication-TileColor" content="#00aba9" />
         <meta name="theme-color" content="#ffffff" />
+        <meta name="civilisation-mamar" content="47.33781761272267, 8.554459710341945" />
         <link rel="icon" type="image/png" href="/favicon32x32.png" />
         <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="/feed.xml" />
         <link rel="alternate" type="application/atom+xml" title="Atom 1.0" href="/atom.xml" />
