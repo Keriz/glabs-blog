4df4b34b80ff4747734049065ceba62f7142a69d
Keriz <guillaume.thivolet@free.fr>
2021-08-06 11:31:20 +0200

    Update logo to the new draft

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,12 @@ export default function Header() {
     <header className="flex justify-between items-center py-10">
       <div>
         <Link href="/">
-          <a aria-label="GLabs Blog">
-            <span className="decoration-slice bg-clip-text bg-gradient-to-b from-red-400 via-yellow-400 to-red-400 text-transparent text-center font-title text-4xl"> GLabs</span>
-          </a>
+          <img src='logo.svg' />
         </Link>
       </div>
       <div>
-        <a  href="https://github.com/Keriz/glabs-blog" aria-label="GLabs Blog Github">
-          <img src='GitHub-Mark-32px.png'/>
+        <a href="https://github.com/Keriz/glabs-blog" aria-label="GLabs Blog Github">
+          <img src='GitHub-Mark-32px.png' />
         </a>
       </div>
     </header>
